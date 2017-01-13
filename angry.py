e7be9ac6db1817761df00f7b5ec4e7cab6c5e52d
Michael Pedruski <mpedruski@gmail.com>
2017-01-13 14:32:20 -0500

    Fully functional first draft of angry.py

--- angry.py
+++ angry.py
@@ -23,18 +23,17 @@ def data_collection(x):
     cur.execute('CREATE TABLE IF NOT EXISTS Names (id INTEGER NOT NULL PRIMARY KEY UNIQUE, name TEXT UNIQUE)')
 
     # Greet the user appropriately
-    print(" \n")
     if entries==0:
-        print("Great, let's begin the entry process:")
+        print("\nGreat, let's begin the entry process:")
     else:
-        print("OK, ready for the next entry:")
+        print("\nOK, ready for the next entry:")
 
     # Collect data from the user for one particular offence
-    print("What's the first name of the person you're mad at?")
+    print("\nWhat's the first name of the person you're mad at?")
     name = input()
-    print("And what did "+name+" do?")
+    print("\nAnd what did "+name+" do?")
     offence = input()
-    print("And how many days do you want to be made at "+name+" for, given this greivous offence?")
+    print("\nAnd how many days do you want to be made at "+name+" for, given this greivous offence?")
     duration = integercheck()
     # Determine expiry date of anger based on current date and desired duration of anger
     exp = datetime.date.today() + datetime.timedelta(days = duration)
@@ -52,30 +51,14 @@ def data_collection(x):
     entries=entries+1
 
     # Determine if that was the last greivance to enter
-    print("Was that you last new greivance (Y/N)")
+    print("\nWas that you last new greivance (Y/N)")
     lg = input()
     while lg != "Y" and lg != "N" and lg != "y" and lg != "n":
         print("Please enter Y or N")
         lg = input()
 
-    # If it was the last offer to review old greivances
-    if lg == "Y" or lg =="y":
-        print("OK, Do you want to see if you have any old greivances still ongoing (Y/N)?")
-        ch = input()
-        while ch != "Y" and ch != "N" and ch != "y" and ch != "n":
-            print("Please enter Y or N")
-            ch = input()
-
-        # If the user wants to review old greivances initiate data report
-        if ch=="Y" or ch=="y":
-            data_report()
-
-        # If the user doesn't want to review old greivances end the program
-        else:
-            print("Goodyyyye")
-
     # If there's another greivance to add recursively call data_collection()
-    elif lg == "N" or lg =="n":
+    if lg == "N" or lg =="n":
         data_collection(entries)
 
 def data_report():
@@ -87,13 +70,13 @@ def data_report():
     for row in cur:
         expdt = datetime.datetime.strptime(row[2],'%Y-%m-%d')
         expdt= datetime.datetime.strftime(expdt,'%b %d, %Y')
-        output = "You're mad at "+ row[0]+" until "+expdt+" for the following crime: "+row[1]
+        output = "\nYou're mad at "+ row[0]+" until "+expdt+" for the following crime: "+row[1]
         print(output)
         count = count+1
     if count >1:
         print("\nWow, you're carrying "+str(count)+" grudges. Maybe you should try taking it easier?")
     elif count == 1:
-        print("1 grudge, eh? Not bad, but you could do better.")
+        print("\n1 grudge, eh? Not bad, but you could do better.")
     else:
         print("No grudges, that's what I like to see!")
     cur.close()
@@ -106,10 +89,12 @@ ng = input()
 while ng != "Y" and ng != "N" and ng != "y" and ng != "n":
     print("Please enter Y or N")
     ng = input()
+# If they have a greivance initiate data_collection(), and regardless of their answer prompt them about a report
 if ng=="Y" or ng=="y":
     data_collection(entries)
+    print("\nAwesome! Do you check on the list of things you're angry about (Y/N)?")
 elif ng=="N" or ng =="n":
-    print("\nAwesome! Do you want to check if you're still mad at somebody for something done and past (Y/N)?\n")
+    print("\nAwesome! Do you check on the list of things you're angry about (Y/N)?")
 ch = input()
 while ch != "Y" and ch != "N" and ch != "y" and ch != "n":
     print("Please enter Y or N")
@@ -119,4 +104,4 @@ if ch=="Y" or ch=="y":
     data_report()
 
 # At the end wish the user a good day
-print("Goodbye!")
+print("\nGoodbye!")
